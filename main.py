dcc938afd9c7fd9d4b1c026f8fea93591adf18ba
Firuz Azizbekov <firuz.azizbekov_2026@ucentralasia.org>
2024-10-13 16:50:58 +0600

    ;

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import random
 
 pygame.init()
 
-screen_width = 800
-screen_height = 600
+screen_width = 1600
+screen_height = 900
+center = [screen_width//2, screen_height//2]
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pygame Starter")
 
@@ -17,20 +18,21 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
-env = Environment(radius=200, x=screen_width//2, y=screen_height//2)
+env = Environment(radius=150, x=center[0], y=center[1])
 
 agents = []
-num_agents = 5
+num_agents = 50
 
 for _ in range(num_agents):
     while True:
         x = random.uniform(env.x-env.radius, env.x+env.radius)
         y = random.uniform(env.y-env.radius, env.y+env.radius)
-        if env.is_inside_circle(x, y):
+        speed_x = random.uniform(0.5, 2)
+        speed_y = random.uniform(0.5, 2)
+        agent = Agent(x, y, speed_x, speed_y)
+        if env.is_inside_circle(agent):
             break
-    speed_x = random.uniform(0.5, 2)
-    speed_y = random.uniform(0.5, 2)
-    agent = Agent(x, y, speed_x, speed_y)
+    
     agents.append(agent)
     
 print("Initial agent positions:")
@@ -43,14 +45,17 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    env.x, env.y = pygame.mouse.get_pos()
+    
+    
+    
     screen.fill(RED)
     
     # Drawing
     pygame.draw.circle(screen, WHITE, (env.x, env.y), env.radius)
     
     for i in agents:
-        pygame.draw.circle(screen, BLACK, (i.x, i.y), 1)
-
+        pygame.draw.circle(screen, BLACK, (i.x, i.y), 3)
 
     pygame.display.flip()
 
