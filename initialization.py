7f09b9f4d478357c931a8514b3fc4d66e1a1e59e
Toktomambetov-Sultan <sultan.toktomambetov.04@bk.ru>
2024-10-13 16:51:11 +0600

    .

--- initialization.py
+++ initialization.py
@@ -2,7 +2,7 @@ from environment import Environment
 from agent import Agent
 import random
 
-environment = Environment(radius=10, center_x=0, center_y=0)
+environment = Environment(radius=10, x=0, y=0)
 
 agents = []
 num_agents = 5
@@ -12,10 +12,11 @@ for _ in range(num_agents):
     while True:
         x = random.uniform(-environment.radius, environment.radius)
         y = random.uniform(-environment.radius, environment.radius)
-        if environment.is_inside_circle(x, y):
+        # Create an agent with random speed
+        speed_x = random.uniform(0.5, 2)
+        speed_y = random.uniform(0.5, 2)
+        agent = Agent(x, y, speed_x, speed_y)
+        if environment.is_inside_circle(agent):
             break
-    # Create an agent with random speed
-    speed_x = random.uniform(0.5, 2)
-    speed_y = random.uniform(0.5, 2)
-    agent = Agent(x, y, speed_x, speed_y)
     agents.append(agent)
+
--- update.py
+++ update.py
@@ -1,6 +1,11 @@
 from initialization import environment, agents
+from agent import Agent
 
-def update(environment=environment, agents=agents):
+reproduce_time = 10
+iteration = 1
+
+def update(environment=environment, agents: list[Agent]=agents):
+    global iteration
     remove_indexes = []
     for i in range(len(agents)):
         agents[i].move()
@@ -8,4 +13,16 @@ def update(environment=environment, agents=agents):
             remove_indexes.append(i)
     for i in remove_indexes:
         del agents[i]
+    iteration += 1
+    if iteration%reproduce_time ==0:
+        new_agents = []
+        for agent in agents:
+            new_agents.append(*agent.reproduce())
+        agents.append(*new_agents) 
     return agents
+
+agents = update()
+print("Initial agent positions:")
+for agent in agents:
+    print(agent)
+
